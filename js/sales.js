67c2f67434732d0fcbcc49948e9814d5e5a70d8e
Alinur Shayakhmet <vashalinur@gmail.com>
2023-10-26 01:11:51 +0600

    another commit

---
+++ js/sales.js
@@ -0,0 +1,30 @@
+function startCountdown(id, endDate) {
+  var countdown = setInterval(function() {
+      var now = new Date().getTime();
+      var distance = endDate - now;
+      if (distance <= 0) {
+          clearInterval(countdown);
+          document.getElementById(id).innerHTML = "The promotion is over";
+      } else {
+          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          document.getElementById(id).innerHTML = days + " : " + hours + " : " + minutes + " : " + seconds + "";
+      }
+  }, 1000);
+}
+
+var countdownDate1 = new Date("2023-11-01 00:00:00").getTime();
+var countdownDate2 = new Date("2023-12-01 00:00:00").getTime();
+var countdownDate3 = new Date("2023-12-15 00:00:00").getTime();
+var countdownDate4 = new Date("2023-11-10 00:00:00").getTime();
+var countdownDate5 = new Date("2023-12-05 00:00:00").getTime();
+var countdownDate6 = new Date("2023-11-20 00:00:00").getTime();
+
+startCountdown("countdown1", countdownDate1);
+startCountdown("countdown2", countdownDate2);
+startCountdown("countdown3", countdownDate3);
+startCountdown("countdown4", countdownDate4);
+startCountdown("countdown5", countdownDate5);
+startCountdown("countdown6", countdownDate6);
---
+++ js/user.js
@@ -0,0 +1,69 @@
+
+if (sessionStorage.getItem('siteCookieAlertShown') !== 'true') {
+    (() => {
+        const y = 'y';
+        const n = 'n';
+
+        alert('By continuing to use our website, you agree to the use of cookies.');
+
+        const input = prompt(`Will you continue? (${y}/${n})`);
+
+        if (input === y) {
+            alert('Enjoy your use! 😉');
+        } else if (input === n) {
+            alert('There may be errors when using our site! 😔');
+        } else {
+            alert('Input error');
+        }
+
+        sessionStorage.setItem('siteCookieAlertShown', 'true');
+    })();
+}
+
+const pass = document.getElementById('pass');
+const signup = document.getElementById('signup');
+const login = document.getElementById('login');
+
+pass.addEventListener("click", () => {
+    if (signup.style.display === "none" && login.textContent === "Reset password") {
+        signup.style.display = "block";
+        login.textContent = "Login";
+    } else {
+        signup.style.display = "none";
+        login.textContent = "Reset password";
+    }
+});
+
+const form = document.getElementById('form');
+const username = document.getElementById('name');
+const password = document.getElementById('password');
+const error = document.getElementById('error');
+
+form.addEventListener('submit', (event) => {
+    let messages = [];
+
+    if (username.value === '' || username.value === null) {
+        messages.push('*Name is required!');
+    }
+
+    if (username.value.length < 4) {
+        messages.push('*Username must be longer than 4 characters!');
+    }
+
+    if (password.value === '' || password.value === null) {
+        messages.push('*Password is required!');
+    }
+
+    if (password.value.length <= 6) {
+        messages.push('*Password must be longer than 6 characters!');
+    }
+
+    if (password.value === 'password') {
+        messages.push('*Password cannot be password!');
+    }
+
+    if (messages.length > 0) {
+        event.preventDefault();
+        error.innerText = messages.join('\n');
+    }
+});
